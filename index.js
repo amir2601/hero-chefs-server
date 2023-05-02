a6bd35aed82192eb5254610dfd26022b9602237b
amir260101~ <amir26hossin@gmail.com>
2023-05-02 16:28:49 +0600

    cors install and data send done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 const chefs = require('./data/chefs.json');
 
@@ -12,11 +15,12 @@ app.get('/chef', (req, res) => {
     res.send(chefs)
 });
 
-app.get('/details/:id', (req, res) => {
+app.get('/chef/:id', (req, res) => {
     const id = req.params.id;
-    const chefInfo = chefs.find(n => n._id == id);
-    res.send(chefInfo);
-})
+    console.log(id);
+    const chefInfo = chefs.find(c => c._id == id);
+    res.send(chefInfo)
+});
 
 app.listen(port, () => {
     console.log(`Chefs API is running on port: ${port}`);

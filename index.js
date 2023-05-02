8bb5d2c3870c34ff1a0ced5ec78d613e537ecec7
amir260101~ <amir26hossin@gmail.com>
2023-05-02 14:24:48 +0600

    params set done

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefs = require('./data/chefs.json');
+
 app.get('/', (req, res) => {
     res.send('Chef is cooking')
 });
+
+app.get('/chef', (req, res) => {
+    res.send(chefs)
+});
+
+app.get('/details/:id', (req, res) => {
+    const id = req.params.id;
+    const chefInfo = chefs.find(n => n._id == id);
+    res.send(chefInfo);
+})
 
 app.listen(port, () => {
     console.log(`Chefs API is running on port: ${port}`);
